bd99e92a3eef6cb5d780dd6bf5a39bee86075b62
praneethpj <ifeathern@gmail.com>
2022-06-23 18:31:46 +0530

    init project

---
+++ src/com/praneethpj/demo/Driver.java
@@ -0,0 +1,18 @@
+package com.praneethpj.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("Praneeth","12", Arrays.asList("80","90","40")));
+        studentList.add(new Student("Oshan","16", Arrays.asList("65","78","90")));
+        studentList.add(new Student("Anjalee","8", Arrays.asList("96","100","46")));
+
+        Stream.of(studentList);
+    }
+}
---
+++ src/com/praneethpj/demo/Student.java
@@ -0,0 +1,39 @@
+package com.praneethpj.demo;
+
+import java.util.List;
+
+public class Student {
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public List getMarks() {
+        return marks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setMarks(List marks) {
+        this.marks = marks;
+    }
+
+    public Student(String name, String age, List marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+
+    String name;
+    String age;
+    List marks;
+}
